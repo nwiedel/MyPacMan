d09e5966d0197d59c3fc23c95602c136a5a44160
nwiedel <164350832+nwiedel@users.noreply.github.com>
2024-09-30 23:36:35 +0200

    Layrinth erstellen Collider Anfang

--- MyPackMan/Assets/Scripts/MovementScript.cs
+++ MyPackMan/Assets/Scripts/MovementScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    public Rigidbody2D rb2d;
+
+    private Vector2 nextDirection;
+
+    public float speed = 0.2f;
+
+    private Vector2 direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,36 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Tasteninput erkennen
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            //nach rechts laufen
+            Debug.Log("nach rechts laufen");
+            nextDirection = Vector2.right;
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            // nach links laufen
+            Debug.Log("nach links laufen");
+            nextDirection = Vector2.left;
+        }
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            // nach oben laufen
+            Debug.Log("nach oben laufen");
+            nextDirection = Vector2.up;
+        }
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            // nach unten laufen
+            Debug.Log("nach unten laufen");
+            nextDirection = Vector2.down;
+        }
+
+        // Richtung berechnen
+        direction = (Vector2)transform.position + nextDirection;
+        Vector2 myPosition = Vector2.MoveTowards(transform.position, direction, speed);
+        // Anweisung, das sich Pacman bewegen soll
+        rb2d.MovePosition(myPosition);
     }
 }
